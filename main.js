163360c98117237d51fac4b17567030f89f195e1
Geovani <maniyt60@gmail.com>
2024-06-14 10:55:56 -0400

    Functional SearchBar Added

--- main.js
+++ main.js
@@ -1,35 +1,30 @@
-let card_botao = document.querySelector("a#card-button")
-let sun = document.getElementById("Sol")
-let mercury = document.getElementById("mercurio")
-let venus = document.getElementById("venus")
-let earth = document.getElementById("terra")
-let mars = document.getElementById("marte")
-let jupiter = document.getElementById("jupiter")
-let saturn = document.getElementById("saturno")
-let uranus = document.getElementById("urano")
-let neptune = document.getElementById("netuno")
-let pagesun = document.getElementById("sun")
-let pagemer = document.getElementById("mercury")
-let pageven = document.getElementById("venus")
-let pageterra = document.getElementById("earth")
-let pagemars = document.getElementById("mars")
-let pagejupiter = document.getElementById("jupiter")
-let pagesat = document.getElementById("saturn")
-let pageur = document.getElementById("uranus")
-let pagenep = document.getElementById("neptune")
+const searchWrapper = document.querySelector(".formsearch");
+const inputBox = searchWrapper.querySelector("input");
+const sugestBox = searchWrapper.querySelector(".list");
+let linkTag = searchWrapper.querySelector("a")
+let weblink;
 
-sun.addEventListener("click", function() {
-    if (pagesun.style.display == "none") {
-        
-    } else {
-        pagesun.style.display = "none"
+let list = [
+    "Sol",
+    "Mercurio",
+    "Venus",
+    "Terra",
+    "Marte",
+    "Jupiter",
+    "Saturno",
+    "Urano",
+    "Netuno"
+]
+
+inputBox.onkeyup = (e) => {
+    let userData = e.target.value;
+    let emptyArray = [];
+    
+    if (e.key === 'Enter') {
+        if (userData) {
+            window.open(`https://manizada.github.io/SpaceY/${userData}.html`);
+        }
     }
-})
-mercury.addEventListener("click", pags())
-venus.addEventListener("click", pags())
-earth.addEventListener("click", pags())
-mars.addEventListener("click", pags())
-jupiter.addEventListener("click", pags())
-saturn.addEventListener("click", pags())
-uranus.addEventListener("click", pags())
-neptune.addEventListener("click", pags())
+}
+
+
