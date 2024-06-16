92f921bdc858b434dca8c0e77c1609ed9bf292fe
Geovani <maniyt60@gmail.com>
2024-06-16 14:25:16 -0400

    Functional search bar added

--- main.js
+++ main.js
@@ -1,66 +1,13 @@
-const searchWrapper = document.querySelector(".formsearch");
+const searchWrapper = document.querySelector(".search");
 const inputBox = searchWrapper.querySelector("input");
-const sugestBox = searchWrapper.querySelector(".list");
-let linkTag = searchWrapper.querySelector("a")
-let weblink;
-
-let list = [
-    "Sol",
-    "Mercurio",
-    "Venus",
-    "Terra",
-    "Marte",
-    "Jupiter",
-    "Saturno",
-    "Urano",
-    "Netuno"
-]
 
 inputBox.onkeyup = (e) => {
     let userData = e.target.value;
-    let emptyArray = [];
-    
-    if (e.key === 'Enter') {
-        if (userData) {
-            window.open(`https://manizada.github.io/SpaceY/${userData}.html`, '_blank');
+    let emptyarray = [];
+
+    if(e.key === "Enter") {
+        if(userData) {
+            window.open(userData + ".html");
         }
     }
-
-    if (userData) {
-        emptyArray = list.filter((data) => {
-            return data.toLowerCase().startsWith(userData.toLocaleLowerCase());
-        });
-        emptyArray = emptyArray.map((data) => {
-            return data = `<li>${data}</li>`
-        })
-        searchWrapper.classList.add('active');
-        showList(emptyArray);
-        let allList = sugestBox.querySelectorAll('li');
-        for (let i = 0; i < allList.length; i++) {
-            allList[i].setAttribute('onclick', 'select(this)');
-        }
-        if (e.key === 'Escape') {
-            searchWrapper.classList.remove('active');
-        } else {
-            searchWrapper.classList.remove('active')
-        }
-    }
-}
-
-function select(element) {
-    let selectData = element.textContent;
-    inputBox.value = selectData; 
-    searchWrapper.classList.remove('active');
-}
-
-function showList (list) {
-    let listData;
-    if (!list.length) {
-        userValue = inputBox.value;
-        listData = `<li>${userData}</li>`
-    } else {
-        listData = list.join('');
-    }
-
-    sugestBox.innerHTML = listData;
 }
